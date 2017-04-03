bdc03a0a4756b8fa5a62bb80728b3c96dbd1eba7
Markus Kobisch <github1@kobisch.net>
2017-04-03 13:28:10 +0200

    Use tabs instead of spaces

--- misc/defaultConfig.js
+++ misc/defaultConfig.js
@@ -2,40 +2,40 @@
 // If such a function doesn't return a string, no command will be executed.
 
 var ftp = {
-    name: 'Upload html files on change via NcFTP',
-    command: function(event, file) {
-        if (event === 'change' && file.match(/\.html$/)) {
-            return 'ncftpput -u user -p password ftp.server.com /srv/http/project "' + file + '"';
-        }
-    }
+	name: 'Upload html files on change via NcFTP',
+	command: function(event, file) {
+		if (event === 'change' && file.match(/\.html$/)) {
+			return 'ncftpput -u user -p password ftp.server.com /srv/http/project "' + file + '"';
+		}
+	}
 };
 
 var rsync = {
-    name: 'Rsync on changes',
-    command: 'rsync -aP --exclude "node_modules" "./" "server:/path/to/destination"'
+	name: 'Rsync on changes',
+	command: 'rsync -aP --exclude "node_modules" "./" "server:/path/to/destination"'
 };
 
 var sass = {
-    command: 'sass --watch css'
+	command: 'sass --watch css'
 };
 
 var config = {
-    directory: '.', // The directory which will be watched for changes. If falsy, the parent directory of this module will be watched.
-    ignore: [ // ignore can be a string, regex, function or an array containing any of them. Has to be anymatch compatible, see https://github.com/es128/anymatch
-        /node_modules/,
-        /\.git/
-    ],
-    delay: 1000,
-    verbosity: 'normal', // Possible values are: minimal, normal, verbose
-    commandsOnStart: [
-        sass
-    ],
-    commandsOnChange: [
-        rsync
-    ],
-    commandsOnEnd: [
+	directory: '.', // The directory which will be watched for changes. If falsy, the parent directory of this module will be watched.
+	ignore: [ // ignore can be a string, regex, function or an array containing any of them. Has to be anymatch compatible, see https://github.com/es128/anymatch
+		/node_modules/,
+		/\.git/
+	],
+	delay: 1000,
+	verbosity: 'normal', // Possible values are: minimal, normal, verbose
+	commandsOnStart: [
+		sass
+	],
+	commandsOnChange: [
+		rsync
+	],
+	commandsOnEnd: [
 
-    ]
+	]
 };
 
 module.exports = config;
